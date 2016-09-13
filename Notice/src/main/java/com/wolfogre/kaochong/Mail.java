494ee1bd18eda9fdc0d90782864817734d69d159
Jason Song <i@wolfogre.com>
2016-09-14 00:30:12 +0800

    Fix recipients

--- Notice/src/main/java/com/wolfogre/kaochong/Mail.java
+++ Notice/src/main/java/com/wolfogre/kaochong/Mail.java
@@ -28,7 +28,7 @@ public class Mail {
     public void init() {
         console.writeLine("Please input password for send email");
         password = console.readLine();
-        sendMail("考虫提醒系统启动", "系统成功启动！", new String[]{"i@wolfogre.com"});
+        sendMail("考虫提醒系统启动", "系统成功启动！", new String[]{"i@wolfogre.com", "wolfogre@qq.com", "wolfogre@163.com"});
     }
 
     public Mail() {
@@ -41,11 +41,19 @@ public class Mail {
 
 
     public void sendMail(String subject, String text, String[] sendTo) {
+        Address[] addresses = new Address[sendTo.length];
+        try {
+            for(int i = 0; i < sendTo.length; ++i)
+                addresses[i] = new InternetAddress(sendTo[i]);
+        } catch (AddressException e) {
+            throw new RuntimeException(e);
+        }
 
         Message msg = new MimeMessage(session);
         try {
             msg.setSubject(subject);
             msg.setText(text);
+            msg.setRecipients(Message.RecipientType.TO, addresses);
             msg.setFrom(new InternetAddress(MimeUtility.encodeText("考虫提醒") + "<kaochong@wolfogre.com>"));
         } catch (MessagingException e) {
             throw new RuntimeException(e);
@@ -56,13 +64,8 @@ public class Mail {
         try{
             Transport transport = session.getTransport();
             transport.connect("kaochong@wolfogre.com", password);
-            Address[] addresses = new Address[sendTo.length];
-            for(int i = 0; i < sendTo.length; ++i)
-                addresses[i] = new InternetAddress(sendTo[i]);
             transport.sendMessage(msg, addresses);
             transport.close();
-        } catch (AddressException e) {
-            throw new RuntimeException(e);
         } catch (NoSuchProviderException e) {
             throw new RuntimeException(e);
         } catch (MessagingException e) {
