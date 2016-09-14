fdefa2321f6dbe3f0521f39db5acc9e2ede07e3a
Jason Song <i@wolfogre.com>
2016-09-14 20:43:09 +0800

    Change plan, extrat to ICS

--- Extractor/src/main/java/com/wolfogre/kaochong/Extrator.java
+++ Extractor/src/main/java/com/wolfogre/kaochong/Extrator.java
@@ -7,6 +7,8 @@ import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+
 /**
  * Created by wolfogre on 9/13/16.
  */
@@ -17,25 +19,44 @@ public class Extrator {
     FileLoader fileLoader;
 
     @Autowired
-    Database database;
-
-    @Autowired
     Console console;
 
-    public void extratToSqlite() {
+    public void extratToICS() {
         Document doc = Jsoup.parse(fileLoader.getOriginHtml());
         Element groups = doc.getElementById("j-groups");
         Elements dataList = groups.getElementsByClass("group_title");
         Elements dataCourseList = groups.getElementsByClass("group_cons_list");
+        console.writeLine("Please input the out path of ics:");
+        FileOutputStream fileOutputStream = null;
+
+        console.writeLine("--------start of ics--------");
+
+        console.writeLine("BEGIN:VCALENDAR");
+        console.writeLine("VERSION:1.0");
+        console.writeLine("PRODID:vCal ID default");
         for(int i = 0; i < dataList.size(); ++i) {
-            String data = dataList.get(i).text();
+            String data =  new StringBuilder(dataList.get(i).text()).deleteCharAt(4).deleteCharAt(6).toString(); // 2016-09-19 -> 20160919
             for(Element e : dataCourseList.get(i).getElementsByClass("group_cons")) {
                 Elements spans = e.select("span");
                 String timeRange = new StringBuilder(spans.get(0).text()).deleteCharAt(6).toString(); // 09:00- 11:00 -> 09:00-11:00
-                database.insertCourse(data + " " + timeRange.split("-")[0], timeRange, spans.get(1).text(), spans.get(2).text());
-                console.writeLine(data + " " + timeRange.split("-")[0] + " | " + timeRange + " | " + spans.get(1).text() + " | " + spans.get(2).text());
+                String startTime = new StringBuilder(timeRange.split("-")[0]).deleteCharAt(2).append("00").toString(); // 09:00 -> 090000
+                String endTime = new StringBuilder(timeRange.split("-")[1]).deleteCharAt(2).append("00").toString(); // 09:00 -> 090000
+                console.writeLine("BEGIN:VEVENT");
+                console.writeLine("DESCRIPTION:狼煞博客 http://blog.wolfogre.com");
+                console.writeLine("DTEND:" + data + "T" + endTime);
+                console.writeLine("DTSTART:" + data + "T" + startTime);
+                console.writeLine("ETZ:Asia/Shanghai");
+                console.writeLine("SUMMARY:【考虫六级】" + spans.get(1).text() + "-" + spans.get(2).text());
+                console.writeLine("REMINDER:15");
+                console.writeLine("REMINDER:3");
+                console.writeLine("OWNER:nobody@localhost");
+                console.writeLine("END:VEVENT");
+
             }
         }
+        console.writeLine("END:VCALENDAR");
+        console.writeLine("--------end of ics--------");
+
 
     }
 }
--- Extractor/src/main/java/com/wolfogre/kaochong/Main.java
+++ Extractor/src/main/java/com/wolfogre/kaochong/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         Extrator extrator = context.getBean(Extrator.class);
-        extrator.extratToSqlite();
+        extrator.extratToICS();
         context.close();
     }
 }
